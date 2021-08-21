c499c8f88e96cbd0b83c61480d8dee0643fcabcb
JosiMF <89228200+JosiMF@users.noreply.github.com>
2021-08-21 16:37:57 -0300

    Add files via upload

---
+++ index.php.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Mapa</title>
+</head>
+<body>
+	<header>
+		<nav>
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="index.php?pagina=quemsomos.php">QuemSomos</a></li>
+				<li><a href="">Produto</a></li>
+				<li><a href="">Contato</a></li>
+			</ul>
+		</nav>
+	</header>
+</body>
+</html>
+
+
