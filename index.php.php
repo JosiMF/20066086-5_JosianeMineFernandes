ef50a1ea5048a641cbc7d36aca900a6487ab84b3
JosiMF <89228200+JosiMF@users.noreply.github.com>
2021-08-21 16:41:12 -0300

    Add files via upload

---
+++ index.php.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Mapa</title>
+</head>
+<body>
+	<header>
+		<nav>
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="index.php?pagina=quemsomos.php">QuemSomos</a></li>
+				<li><a href="">Produto</a></li>
+				<li><a href="">Contato</a></li>
+			</ul>
+		</nav>
+	</header>
+	<main>
+		<?php
+			if (isset ($_GET ["pagina"])) && !empty ($_GET["pagina"]){
+				$pagina=
+			} else {
+				
+			}
+</body>
+</html>
+
+
