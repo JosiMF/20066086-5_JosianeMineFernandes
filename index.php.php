ab24584360af6ca29ffbdc830d879de303031fd7
JosiMF <89228200+JosiMF@users.noreply.github.com>
2021-08-21 21:38:45 -0300

    Add files via upload

--- index.php.php
+++ index.php.php
@@ -1,27 +1,92 @@
 <!DOCTYPE html>
-<html>
+<html lang="pt-br">
 <head>
-	<title>Mapa</title>
-</head>
-<body>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name= "viewport" content="width=device-width, initial-scale=1.0">
+	<title>Doces da Vovó</title>
+	<style>
+		body{
+			margin: 0;
+			font-family: 'arial';
+		}
+		header{
+			height: 180px;
+			background-color: hotpink;
+			text-align: center;
+			padding: 10px;
+		}
+	.logo{
+		height: 170px;
+	}
+
+		nav{
+			background-color: lightpink;
+
+		}
+		nav ul{
+			list-style: none;
+			width: 100%;
+			margin:  0px;
+			padding: 0;
+			display:  flex;
+			justify-content: center;
+		}
+
+		nav ul li{
+			padding-top: 15px;
+			padding-left: 30px;
+			padding-right: 30px;
+			padding-bottom: 15px ;
+		}
+
+		.content{
+			min-height: 450px;
+		}
+
+		footer{
+			background-color: lightpink;
+			min-height: 50px;
+			padding: 20px ;
+			margin:  0px;
+			text-align: center;
+		}
+		.container{
+			max-width: 800px;
+			display:  flex;
+			justify-content: space-between ;
+			margin:  auto;
+			padding-top: 35px;
+			flex-wrap: wrap;
+		}
+
+</style>
 	<header>
-		<nav>
-			<ul>
-				<li><a href="index.php">Home</a></li>
-				<li><a href="index.php?pagina=quemsomos.php">QuemSomos</a></li>
-				<li><a href="">Produto</a></li>
-				<li><a href="">Contato</a></li>
-			</ul>
-		</nav>
+		<img src= "images/logo.jpg" alt="logo" class="logo">
 	</header>
-	<main>
+	<nav>
+		<ul>
+			<li>Home</li>
+			<li>Quem Somos</li>
+			<li>Contatos</li>
+			<li>Localização</li>
+		</ul>
+	</nav>
+	<div class="content">
+		<div class="container">
 		<?php
-			if (isset ($_GET ["pagina"])) && !empty ($_GET["pagina"]){
-				$pagina=
-			} else {
-				
-			}
-</body>
+			include ('pages/home.php');
+		?>
+		</div>
+	</div>
+
+	<footer>
+		Josiane Mine Fernandes <br>
+		RA: 20066086-5<br>
+		Curso: Sistemas para Internet<br>
+		UNICESUMAR
+	</footer>
+	</body>
 </html>
 
 
