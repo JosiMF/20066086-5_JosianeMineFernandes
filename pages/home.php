a9ef7fd7480df4f005fd2540a204cd274560a111
JosiMF <89228200+JosiMF@users.noreply.github.com>
2021-08-21 21:37:57 -0300

    Add files via upload

---
+++ pages/home.php
@@ -0,0 +1,87 @@
+<style>
+	.produto1 img{
+		width: 200px;
+		heigth: 200px;
+		border-radius: 100px;	
+	}
+	.produto1{
+		text-align:center ;
+		width: 200px;
+		margin:  30px;
+	}
+	.produto2 img{
+		width: 200px;
+		heigth: 200px;
+		border-radius: 100px;	
+	}
+	.produto2{
+		text-align:center ;
+		width: 200px;
+		margin:  30px;
+	}
+	.produto3 img{
+		width: 200px;
+		heigth: 200px;
+		border-radius: 100px;	
+	}
+	.produto3{
+		text-align:center ;
+		width: 200px;
+		margin:  30px;
+	}
+	.produto4 img{
+		width: 200px;
+		heigth: 200px;
+		border-radius: 100px;	
+	}
+	.produto4{
+		text-align:center ;
+		width: 200px;
+		margin:  30px;
+	}
+	.produto5 img{
+		width: 200px;
+		heigth: 200px;
+		border-radius: 100px;	
+	}
+	.produto5{
+		text-align:center ;
+		width: 200px;
+		margin:  30px;
+	}
+	.produto6 img{
+		width: 200px;
+		heigth: 200px;
+		border-radius: 100px;	
+	}
+	.produto6{
+		text-align:center ;
+		width: 200px;
+		margin:  30px;
+	}
+</style>
+
+<div class="produto1">
+	<img src="images/doce.jpg" alt="docedeleite">
+	<h3>Doce de Leite</h3>
+</div>
+<div class="produto2">
+	<img src="images/abacaxi.jpg" alt="abacaxi">
+	<h4>Abacaxi</h4>
+</div>
+<div class="produto3">
+	<img src="images/abobora.jpg" alt="abobora">
+	<h5>Abóbora</h5>
+</div>
+<div class="produto4">
+	<img src="images/laranja.jpg" alt="laranja">
+	<h6>Laranja</h6>
+</div>
+<div class="produto5">
+	<img src="images/morango.jpg" alt="morango">
+	<h7>Morango</h7>
+</div>
+<div class="produto6">
+	<img src="images/goiaba.jpg" alt="goiaba">
+	<h8>Goiaba</h8>
+</div>
