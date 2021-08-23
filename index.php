ab7c69e5765eac41a967dfad72a6f5270112d3e5
JosiMF <89228200+JosiMF@users.noreply.github.com>
2021-08-23 19:47:58 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" contet="IE=edge">
+	<meta name="viewport" content= "width=device-width, initial-scale=1.0">
+	<title>Doces da VoVó</title>
+</head>
+<style>
+	header{
+		height: 150px;
+		background-color: hotpink;
+
+	}
+</style>
+<body>
+	<header>
+		<img src="images/logo.jpg" alt="logo">
+	</header>
+	<nav>
+
+	</nav>
+	<div>
+
+	</div>
+	<footer>
+
+	</footer>
+
+</body>
+</html>
