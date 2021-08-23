c33fcb7060dd1b476c4ba6d5ccdd74de70d9bfdf
JosiMF <89228200+JosiMF@users.noreply.github.com>
2021-08-22 21:08:12 -0300

    Add files via upload

--- pages/home.php
+++ pages/home.php
@@ -1,87 +1,31 @@
+<?php
+	require_once('dados/produtos.php');
+?>
+
 <style>
-	.produto1 img{
+	.card-produto img{
 		width: 200px;
-		heigth: 200px;
-		border-radius: 100px;	
+		height: 200px;
+		border-radius: 100px;
 	}
-	.produto1{
-		text-align:center ;
+	card-produto{
+		text-align: center;
 		width: 200px;
-		margin:  30px;
-	}
-	.produto2 img{
-		width: 200px;
-		heigth: 200px;
-		border-radius: 100px;	
-	}
-	.produto2{
-		text-align:center ;
-		width: 200px;
-		margin:  30px;
-	}
-	.produto3 img{
-		width: 200px;
-		heigth: 200px;
-		border-radius: 100px;	
-	}
-	.produto3{
-		text-align:center ;
-		width: 200px;
-		margin:  30px;
-	}
-	.produto4 img{
-		width: 200px;
-		heigth: 200px;
-		border-radius: 100px;	
-	}
-	.produto4{
-		text-align:center ;
-		width: 200px;
-		margin:  30px;
-	}
-	.produto5 img{
-		width: 200px;
-		heigth: 200px;
-		border-radius: 100px;	
-	}
-	.produto5{
-		text-align:center ;
-		width: 200px;
-		margin:  30px;
-	}
-	.produto6 img{
-		width: 200px;
-		heigth: 200px;
-		border-radius: 100px;	
-	}
-	.produto6{
-		text-align:center ;
-		width: 200px;
-		margin:  30px;
+		font: arial, 20;
+		margin: 40px;
+
 	}
 </style>
 
-<div class="produto1">
-	<img src="images/doce.jpg" alt="docedeleite">
-	<h3>Doce de Leite</h3>
+<?php
+	foreach ($produtos as $produtos):
+?>
+
+<div class="card-produto">
+	<img src="images/<?php echo $produtos['imagem']?>" alt="<?php echo $produtos['nome']?>">
+		<h3><?php echo $produtos['nome']?></h3>
+		
 </div>
-<div class="produto2">
-	<img src="images/abacaxi.jpg" alt="abacaxi">
-	<h4>Abacaxi</h4>
-</div>
-<div class="produto3">
-	<img src="images/abobora.jpg" alt="abobora">
-	<h5>Abóbora</h5>
-</div>
-<div class="produto4">
-	<img src="images/laranja.jpg" alt="laranja">
-	<h6>Laranja</h6>
-</div>
-<div class="produto5">
-	<img src="images/morango.jpg" alt="morango">
-	<h7>Morango</h7>
-</div>
-<div class="produto6">
-	<img src="images/goiaba.jpg" alt="goiaba">
-	<h8>Goiaba</h8>
-</div>
+<?php
+	endforeach
+?>
