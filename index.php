b4aae40b56cc2b95f663564428090401389fedb1
JosiMF <89228200+JosiMF@users.noreply.github.com>
2021-08-23 20:33:08 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -4,26 +4,70 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" contet="IE=edge">
 	<meta name="viewport" content= "width=device-width, initial-scale=1.0">
-	<title>Doces da VoVó</title>
-</head>
+	<title>Doces da Vovó</title>
 <style>
+	body{
+		margin: 0;
+	}
 	header{
 		height: 150px;
 		background-color: hotpink;
+		text-align: center;
+		paddin: 25px;
 
 	}
+	.logo{
+		height: 130px;
+	}
+	nav{
+		background-color: lightpink;
+
+	}
+	nav ul{
+		list-style: none;
+		width: 100%;
+		margin: 0px;
+		padding: 0;
+		display: flex;
+		justify-content: center;
+	}
+	nav ul li{
+		padding: 12px;
+		padding-left: 25px;
+		padding-right: 25px;
+		padding-top: 12px;
+	}
+	.content{
+		min-height: 400px;
+	}
+	footer{
+		background-color: lightpink;
+		min-height: 50px;
+		padding: 40px;
+		justify-content: center;
+	}
 </style>
+</head>
 <body>
 	<header>
-		<img src="images/logo.jpg" alt="logo">
+		<img src="images/logo.png" alt="logo" class="logo">
 	</header>
 	<nav>
-
+		<ul>
+			<li>Home</li>
+			<li>Quem Somos</li>
+			<li>Contato</li>
+			<li>Localização</li>
+		</ul>
 	</nav>
-	<div>
+	<div class="content">
 
 	</div>
 	<footer>
+		Josiane Mine Fernandes<br>
+		RA: 20066086-5 <br>
+		Curso: Sistemas para Internet
+
 
 	</footer>
 
