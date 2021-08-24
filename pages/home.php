0daf94ba58f2b0122aab0c7c6cfd9c97ae0b908e
JosiMF <89228200+JosiMF@users.noreply.github.com>
2021-08-23 21:43:36 -0300

    Add files via upload

--- pages/home.php
+++ pages/home.php
@@ -1,38 +1,32 @@
+<?php
+	require_once('dados/produtos.php');
+?>
+
 <style>
-	.card-produto{
-		width: 200px;
-		text-align: center;
-		margin-left: 30px;
-		margin-right: 30px;
-		margin-top: 30px;
-	}
 	.card-produto img{
 		width: 200px;
 		height: 200px;
 		border-radius: 100px;
 	}
+	card-produto{
+		text-align: center;
+		width: 200px;
+		font: arial, 20;
+		margin: 40px;
+
+	}
 </style>
-<div class="card-produto">
-	<img src="images/doce.jpg" alt="doce">
-	<h3>Doce de Leite</h3>
-</div>
-<div class="card-produto">
-	<img src="images/abacaxi.jpg" alt="abacaxi">
-	<h3>Abacaxi com Coco</h3>
-</div>
-<div class="card-produto">
-	<img src="images/goiaba.jpg" alt="goiaba">
-	<h3>Goiabada</h3>
-</div>
-<div class="card-produto">
-	<img src="images/abobora.jpg" alt="abobora">
-	<h3>Doce de Leite</h3>
-</div>
-<div class="card-produto">
-	<img src="images/morango.jpg" alt="morango">
-	<h3>Geléia de Morango</h3>
-</div>
-<div class="card-produto">
-	<img src="images/laranja.jpg" alt="laranja">
-	<h3>Laranja</h3>
-</div>
+
+<?php
+	foreach($produtos as $produtos):
+?>
+
+<a href="index.php?page=produtos&id=<?php echo $produto['id']?>"
+	<div class="card-produto">
+		<img src="images/<?php echo $produtos['imagem']?>" alt="<?php echo $produtos['nome']?>">
+			<h3><?php echo $produtos['nome']?></h3>
+		
+	</div>
+<?php
+	endforeach
+?>
