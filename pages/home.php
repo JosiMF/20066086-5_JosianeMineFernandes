207c43bb8ce6b1d325fab5050ad52d6081163d5d
JosiMF <89228200+JosiMF@users.noreply.github.com>
2021-08-25 18:36:43 -0300

    Add files via upload

--- pages/home.php
+++ pages/home.php
@@ -1,23 +1,24 @@
-<?php
-	require_once ('dados/produtos.php');
-?>
-
-<style>
-	.card-produto img{
-		width: 200px;
-		heigt: 200px;
-		border-radius: 100px;
-	}
-	.card-produto{
-		width: 200px;
-		text-align: center;
-		margin: 30px;
-	}
-</style>
-
-<?php
-	foreach($produtos as $produtos)
-<div class="card-produto">
-	<img src="images/<?php echo $produto['imagem']?>" alt="<?php echo $produto ['nome']">
-	<h3><?php echo $produto ['nome']?></h3>
+<div class="doce">
+	<img src="images/doce.png" alt="doce">
+	<h3>Doce de Leite</h3>
+</div>
+<div class="doce">
+	<img src="images/abobora.png" alt="abobora">
+	<h3>Abóbora</h3>
+</div>
+<div class="doce">
+	<img src="images/abacaxi.png" alt="abacaxi">
+	<h3>Abacaxi com coco</h3>
+</div>
+<div class="doce">
+	<img src="images/goiaba.png" alt="goiaba">
+	<h3>Goiaba</h3>
+</div>
+<div class="doce">
+	<img src="images/laranja.png" alt="laranja">
+	<h3>Doce de laranja</h3>
+</div>
+<div class="doce">
+	<img src="images/morango.png" alt="morango">
+	<h3>Morango</h3>
 </div>
--- pages/produtos.php
+++ pages/produtos.php
@@ -1,68 +1,42 @@
+<style>
+	.grid {
+		display: grid;
+		grid-template-columns: auto auto auto;
+		width: 100%;
+	}
+	.coluna {
+		border: 1px solid #ccc;
+		margin: 10px;
+		padding: 10px;
+		text-align: center;
+	}
+	.coluna img {
+		width: 100%;
+	}
+</style>
+<h1>Produtos</h1>
+<div class="grid">
 <?php
+	require_once('dados/produtos.php');
 
-$produtos['docedeleite'] = array(
-'nome'=> 'Doce de Leite',
-'descricao'=> 'Doce de leite feito com ingredientes selecionados.
-<br/>
-Ingredientes: Leite, açúcar e leite condensado.
-<br/>
-Peso 300g
-<br>
-Preço R$ 10,00',
-'imagem'=> 'doce.png'
-);
-$produtos['abóbora'] = array(
-'nome'=> 'Doce de Abóbora',
-'descricao'=> 'Doce de Abóbora feito com ingredientes selecionados.
-<br/>
-Ingredientes: Abóbora, açúcar, coco ralado, cravo da índia.
-<br/>
-Peso 300g
-Preço R$ 8,50',
-'imagem'=> 'abobora.png'
-);
-$produtos['abacaxi'] = array(
-'nome'=> 'Abacaxi com coco',
-'descricao'=> 'Doce de Abacaxi feito com ingredientes selecionados e frescos.
-<br/>
-Ingredientes: Abacaxi, açúcar, coco ralado e cravo da índia.
-<br/>
-Peso 300g
-<br>
-Preço R$ 9,00',
-'imagem'=> 'abacaxi.png'
-);
-$produtos['goiaba'] = array(
-'nome'=> 'Doce de Goiaba',
-'descricao'=> 'Doce de Goiaba feito com ingredientes selecionados.
-<br/>
-Ingredientes: Goiaba e açúcar.
-<br/>
-Peso 300g
-<br>
-Preço R$ 8,00',
-'imagem'=> 'goiaba.png'
-);
-$produtos['laranja'] = array(
-'nome'=> 'Doce de Laranja',
-'descricao'=> 'Doce de Laranja feito com laranjas colhidas diretamente do pomar da vovó.
-<br/>
-Ingredientes: Laranja, açúcar.
-<br/>
-Peso 300g
-<br>
-Preço R$ 8,00',
-'imagem'=> 'laranja.png'
-);
-$produtos['morango'] = array(
-'nome'=> 'Geléia de Morango',
-'descricao'=> 'Geléia feita com morangos orgânicos.
-<br/>
-Ingredientes: Morangos, açúcar, limão.
-<br/>
-Peso 300g
-<br>
-Preço R$ 12,00',
-'imagem'=> 'morango.png'
-);
+	foreach ($produtos as $p) {
+		$nome = $p['nome'];
+		$imagem = $p['imagem'];
+		$id = $p['id'];
+
+		?>
+		<div class="coluna">
+			<img src="images/<?php echo $imagem;?>">
+			<p><strong><?php echo $nome;?></strong></p>
+			<p>
+				<a href="index.php?page=produto&id=<?php echo $id;?>">
+					Detalhes
+				</a>
+			</p>
+		</div>
+		<?php 
+
+	}
+	?>
+</div>
 
