d5018296fa4f399872d341bf4c6c6085156c42e9
JosiMF <89228200+JosiMF@users.noreply.github.com>
2021-08-25 20:33:21 -0300

    Add files via upload

---
+++ pages/home.php
@@ -0,0 +1,42 @@
+<style>
+	.card-produto img{
+		width: 200px;
+		height: 200px;
+		border-radius: 100px;
+	}
+	.card-produto{
+		width: 200px;
+		text-align: center;
+		margin-left: 30px;
+		margin-right: 30px;
+	}
+</style>
+
+
+<div class="card-produto">
+	<img src="images/doce.png" alt="doce">
+	<h3>Doce de Leite</h3>
+</div>
+
+<div class="card-produto">
+	<img src="images/abacaxi.png" alt="abacaxi">
+	<h3>Abacaxi com Coco</h3>
+</div>
+
+<div class="card-produto">
+	<img src="images/abobora.png" alt="abobora">
+	<h3>Doce de abóbora</h3>
+</div>
+
+<div class="card-produto">
+	<img src="images/laranja.png" alt="laranja">
+	<h3>Laranja</h3>
+</div>
+<div class="card-produto">
+	<img src="images/morango.png" alt="morango">
+	<h3>Geléia de Morando</h3>
+</div>
+<div class="card-produto">
+	<img src="images/goiaba.png" alt="goiaba">
+	<h3>Goiaba</h3>
+</div>
