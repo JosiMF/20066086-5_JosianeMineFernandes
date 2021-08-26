79df85a3af27ab6a89959ed10bab1637a86aaca9
JosiMF <89228200+JosiMF@users.noreply.github.com>
2021-08-26 18:41:52 -0300

    Add files via upload

--- pages/localizacao.php
+++ pages/localizacao.php
@@ -1,8 +1,8 @@
 <div class="prod-cl">
-	<img src="images/mapa.png">
+	<img src="images/mapa1.png">
 </div>
 <div class="prod-cr">
-	<h4>Localização</h4>
+	<h2>Localização</h2>
 	<p>
 		Avenida Laguna, 850, Vila Operária, Maringá-PR.
 	</p>
@@ -11,3 +11,13 @@
 		Whatsapp: (44) 99999 9000
 	</p>
 </div>
+<style>
+		.prod-cl img{
+		width: 800px;
+		height: 400px;
+	}
+	. prod-cr{
+		width: 50%;
+		margin-bottom: 10px;
+	}
+</style>
