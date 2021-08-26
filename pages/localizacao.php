4be5cd7018419e7a6c859e120cc1715590352996
JosiMF <89228200+JosiMF@users.noreply.github.com>
2021-08-25 23:33:58 -0300

    Add files via upload

---
+++ pages/localizacao.php
@@ -0,0 +1,13 @@
+<div class="prod-cl">
+	<img src="images/mapa.png">
+</div>
+<div class="prod-cr">
+	<h4>Localização</h4>
+	<p>
+		Avenida Laguna, 850, Vila Operária, Maringá-PR.
+	</p>
+	<p>
+		Telefone: (44) 3030 0000
+		Whatsapp: (44) 99999 9000
+	</p>
+</div>
